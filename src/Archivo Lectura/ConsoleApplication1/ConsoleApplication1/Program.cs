d301bc0755332314411bb6cf592b9426eda59a44
MechMax <alejoemisztal@yahoo.com.ar>
2019-10-22 12:43:14 -0300

    reemplazar lineas de un archivo

--- src/Archivo Lectura/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ src/Archivo Lectura/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,45 +10,75 @@ namespace ConsoleApplication1
     {
         //static readonly string textFile = @"C:\Users\alumno\Desktop\file0.txt";
 
-        static readonly string textFile = @"C:\Users\alumno\UNDERTALE\file01.odt";
+        // static readonly string textFile = @"C:\Users\alumno\UNDERTALE\file01.odt";
+            
 
         static void Main(string[] args)
         {
+
+            string textFile = @"C:\Users\alumno\UNDERTALE\file01.odt";
+
             string salir = "n";
             do{
-            Console.Clear();
-            Console.WriteLine("Introduzca lo que desea buscar: ");
-            String texto;
-            texto = Console.ReadLine();
 
-            int i = 0;
-            int cont = 0;
-            string[] lines = File.ReadAllLines(textFile);
-            foreach (string line in lines)
-            {
-                i++;
-                string aux = line;
-                aux = aux.ToLower();
-                if (aux.IndexOf(texto) != -1)
+                Console.Clear();
+                Console.WriteLine("Introduzca lo que desea cargar: ");
+                String texto;
+                texto = Console.ReadLine();
+
+                int cont = 0;
+                string[] lines = File.ReadAllLines(textFile);
+                foreach (string line in lines)
                 {
+
                     cont++;
+                    string aux = line;
+                    aux = aux.ToLower();
+                    string archivoTexto = File.ReadAllText(textFile);
+                    archivoTexto = archivoTexto.Replace(aux, texto);
+                    File.WriteAllText(@"C:\Users\alumno\UNDERTALE\file01.odt", archivoTexto);
+
                 }
 
-                       /* using (StreamWriter file = new StreamWriter(@"C:\Users\alumno\UNDERTALE\file01.odt", true))
-                        {
-                            file.WriteLine("ANDA"); //se agrega información al documento
+                // int i = 0;
+                //int cont = 0;
+                /* string[] lines = File.ReadAllLines(textFile);
+                 foreach (string line in lines)
+                 {
+                     cont++;
+                     string aux = line;
+                     aux = aux.ToLower();
+                     if (aux.IndexOf(texto) != -1)
+                     {
+                         cont++;
+                     }
 
-                            file.Close();
-                        }
-                       */
+                         
+
+
+
+                         /* using (StreamWriter file = new StreamWriter(@"C:\Users\alumno\UNDERTALE\file01.txt", true))
+                              {
+                                  file.WriteLine("ANDA"); //se agrega información al documento
+
+                                  file.Close();
+                              }
+                              */
+                /* using (StreamWriter file = new StreamWriter(@"C:\Users\alumno\UNDERTALE\file01.odt", true))
+                 {
+                     file.WriteLine("ANDA"); //se agrega información al documento
+
+                     file.Close();
+                 }
+
 
             }
             Console.WriteLine("Lineas total leidas: "+i+" / Coincidencias: "+cont);
 
-            
+         */
 
-            Console.WriteLine("Desea salir? s/n ");
-            salir = Console.ReadLine();
+                Console.WriteLine("Desea salir? s/n ");
+                salir = Console.ReadLine();
 
             }while( salir != "s");
         }
