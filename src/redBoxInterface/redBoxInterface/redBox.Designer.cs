f0054becad0b77086e4c224c92f568cf9ab1d585
alguien <some@mail.com>
2019-10-22 12:27:54 -0300

    Probando como hacer para cerrar procesos

--- src/redBoxInterface/redBoxInterface/redBox.Designer.cs
+++ src/redBoxInterface/redBoxInterface/redBox.Designer.cs
@@ -72,6 +72,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "PLAY";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // redBox
             // 
--- src/redBoxInterface/redBoxInterface/redBox.cs
+++ src/redBoxInterface/redBoxInterface/redBox.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -32,5 +33,18 @@ namespace redBoxInterface
 
         }
 
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (var process in Process.GetProcesses())
+            {
+                if( process.ProcessName == "Taskmgr")
+                {
+
+                    process.Kill();
+
+                }
+            }
+        }
     }
 }
